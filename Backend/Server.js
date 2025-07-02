0f1a7bf595bd108147cc4e0b0f8cacc627d5a4af
shanya.shrivastava.06@gmail.com <154894038+Shanyaaaa@users.noreply.github.com>
2025-07-02 16:35:26 +0530

    middleware created

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
 
 app.get('/', (req, res) => {
   res.send("API working");
--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -2,3 +2,61 @@
 
 
 //FUNCTION FOR ADD PRODUCT
+const addProduct = async (req, res) => {
+    try{
+        const{ name,price,description,category,conditions,bestseller }=req.body;
+        const image1 = req.files.image1[0]
+        const image2 = req.files.image2[0]
+        const image3 = req.files.image3[0]
+        const image4 = req.files.image4[0]
+
+        console.log(name,price,description,category,conditions,bestseller )
+        console.log(image1, image2, image3, image4)
+        
+        res.json({})
+    } catch (error) {
+        res.json({success:false,message:error.message})
+}
+}
+
+
+// FUNCTION FOR LIST PRODUCTS
+const listProducts = async (req, res) => {
+    // try{
+    //     const products = await productModel.find({});
+    //     res.json({success:true, products})
+    // }
+    // catch (error){
+    //     console.log(error);
+    //     res.json({success:false, message: error.message})
+    // }
+}
+
+
+// FUNCTION FOR REMOVE PRODUCT
+const removeProduct = async (req, res) => {
+    // try {
+    //     await productModel.findByIdAndDelete(req.body.id)
+    //     res.json({ success: true, message: "Product removed"})
+    // }
+    // catch (error) {
+    //     console.log(error);
+    //     res.json({ success: false, message: error.message })
+    // }
+}
+
+// FUNCTION FOR SINGLE PRODUCT INFO
+const singleProduct = async (req, res) => {
+    // try {
+    //     const { productId } = req.body
+    //     const product = await productModel.findById(productId)
+    //     res.json({success: true, product})
+    // }
+    // catch (error){
+    //     console.log(error)
+    //     res.json({success:false, message: error.message})
+    // }
+}
+
+
+export { addProduct, listProducts, removeProduct, singleProduct }
