582cdb2decf291075fc312e15ebaa9557bdc684f
shanya.shrivastava.06@gmail.com <154894038+Shanyaaaa@users.noreply.github.com>
2025-07-02 15:44:38 +0530

    created registeration

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
 
 dotenv.config();
 
@@ -10,9 +12,12 @@ const port = process.env.PORT || 4000;
 
 
 connectDB();
+connectCloudinary()
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/user', userRouter)
 
 app.get('/', (req, res) => {
   res.send("API working");
