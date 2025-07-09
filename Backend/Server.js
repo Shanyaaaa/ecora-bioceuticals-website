f4c7007cb0633aeacbcd7905c8c18c6d387c24c8
shanya.shrivastava.06@gmail.com <154894038+Shanyaaaa@users.noreply.github.com>
2025-07-09 13:38:48 +0530

    Server initialised

--- Backend/Server.js
+++ Backend/Server.js
@@ -11,15 +11,17 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
-
 connectDB();
-connectCloudinary()
+connectCloudinary();
 
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
-app.use('/api/user', userRouter)
-app.use('/api/product', productRouter)
+// ✅ Serve images statically
+app.use('/uploads', express.static('uploads'));
+
+app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
 
 app.get('/', (req, res) => {
   res.send("API working");
